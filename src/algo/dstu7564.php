b5d3b7e94ab44d79dfca640d0f322b598a49ecd2
leon.mbs@gmail.com <leon.mbs@gmail.com>
2026-08-21 19:24:04 +0300

    fix

--- src/algo/dstu7564.php
+++ src/algo/dstu7564.php
@@ -5,11 +5,11 @@ namespace PPOLib\Algo;
 use PPOLib\Util;
 
 /**
-* хеширование  согласно ДСТУ 7564
+* хеширование  согласно ДСТУ 7564  (Купына)
 * портировано с  https://github.com/storojs72/bc-dstu-csharp
 */
 
-/*
+
 class DSTU7564
 {
     public const ROWS = 8;
@@ -397,6 +397,15 @@ class DSTU7564
         return $padded;
     }
 
+    
+    /**
+    * проверка
+    *
+    *  $test= \PPOLib\Util::hex2array("000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F") ;
+    *  $expected= \PPOLib\Util::hex2array(08F4EE6F1BE6903B324C4E27990CB24EF69DD58DBE84813EE0A52F6631239875") ;
+    * 
+    * @param mixed $data
+    */
     public static function hash($data) {
         if (is_string($data)) {
             $data = Util::str2array($data);
@@ -411,4 +420,4 @@ class DSTU7564
     }
 
 }
-*/
+
